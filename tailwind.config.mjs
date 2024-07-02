9abc80f8bb6e5a5550136dcf2753be2ae3134799
Morelo <Morelao.jm@gmail.com>
2024-07-02 17:30:09 -0500

    feat: Update banner image and add menu toggle functionality

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}','./node_modules/flowbite/**/*.js'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage:{
+				"close-menu":"url('src/images/icon-close.svg')",
+				"open-menu":"url('src/images/icon-hamburger.svg')",
+			}
+		},
 	},
 	plugins: [
 		require('flowbite/plugin')
